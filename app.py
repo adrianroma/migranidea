68d1dd15434e30021074a252cc5eed537b1870e9
adrianomnipro <143022803+adrianomnipro@users.noreply.github.com>
2026-02-18 15:09:22 -0600

    Update app.py
    sample

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
-from flask_cors import CORS
+from flask_cors import cross_origin
 
 app = Flask(__name__, template_folder="templates")
 
 
-CORS(app, resources={r"*": {"origins": "*"}})
+# Source - https://stackoverflow.com/a/76508341
+# Posted by imran3
+# Retrieved 2026-02-18, License - CC BY-SA 4.0
+
+
 
 @app.route("/")
-
+@cross_origin(origins=["https://unpkg.com", "https://esm.sh"])
 def index():
     return render_template("/index.html")
 
